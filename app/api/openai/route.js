a291028a61dfa5045f3bc6f46ce999d5747f4c91
kaleabtk <kaleab.tekalegn@gmail.com>
2025-12-16 00:12:23 +0300

    AI-powered grammar checker

---
+++ app/api/openai/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai"
+
+const token = process.env.TOKEN
+const endpoint = process.env.ENDPOINT
+const model = process.env.MODEL
+
+export async function POST(request) {
+
+    // const { inputValue } = await request.json();
+    const body = await request.json()
+
+    const client = new OpenAI({ baseURL: endpoint, apiKey: token });
+
+    const response = await client.chat.completions.create({
+        model: model,
+        messages: [
+            { role: "system", content: "" },
+            { role: "user", content: `Only check this for grammar and spelling: ${body.input_text}` }
+        ],
+    });
+
+    return NextResponse.json(
+        { response: response.choices[0].message.content }
+    )
+
+}
--- app/page.js
+++ app/page.js
@@ -1,65 +1,59 @@
-import Image from "next/image";
+'use client'
+import "./globals.css";
+import { useState } from 'react'
+import ReactMarkdown from 'react-markdown';
 
-export default function Home() {
+function App() {
+
+  const [inputValue, setInputValue] = useState('')
+  const [response, setResponse] = useState('')
+  const [loading, setLoading] = useState('')
+
+
+  const onChangeHandler = (event) => {
+    const value = event.target.value
+    setInputValue(value)
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    setLoading('Loading...')
+
+    await fetch('/api/openai', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input_text: inputValue })
+    }).then((res) => {
+      if (!res.ok) throw new Error("Request failed!");
+      return res.json();
+    })
+      .then((json) => setResponse(json.response))
+      .catch(console.error)
+      .finally(() => setLoading(''));
+
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <div className='flex flex-col items-center gap-3 mt-3 w-[96%] md:w-[75%] lg:w-[50%] m-auto'>
+      <p className='mt-3.5'>Enter or paste your sentence to check its grammar and spelling.</p>
+
+      <div className='w-full relative'>
+        <form onSubmit={submitHandler} className='flex justify-center items-center flex-col gap-3'>
+
+          <textarea onChange={onChangeHandler} value={inputValue} name="input_text" id="input_text"
+            className='border border-[#b09e99aa] focus:outline-none w-full h-25 rounded-md px-1 py-0.5 bg-[#302e2e]'></textarea>
+          <button className='cursor-pointer absolute right-2 bottom-11' onClick={() => setInputValue('')} disabled={inputValue === ''}>Clear</button>
+
+          <button className='bg-[#3c3636] font-bold px-1.5 py-1 rounded-md cursor-pointer hover:bg-[#463b3b]' type="submit">Check</button>
+        </form>
+      </div>
+
+      <div className='border border-[#b09e99aa] w-full min-h-4 rounded-md p-2.5'>{loading}
+        <ReactMarkdown>{response}</ReactMarkdown>
+      </div>
+    </div >
+  )
 }
+
+export default App
